2332dcbeaa4566831789cb527150849bec496139
Alexi <you@example.com>
2023-07-19 19:52:49 -0700

    Resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,8 @@ export default function Headers() {
 				A passionate developer with expertise in Unity and React.
 				I love turning ideas into reality through
 				creative problem-solving and innovative solutions. Over the past 2 years, I have worked
-				on various projects, honing my skills and delivering exciting outcomes
+				on various projects, honing my skills and delivering exciting outcomes. If you're looking for a creative and enthusiastic developer, 
+				let's connect and build amazing projects together!
 			</div>
 			<img src={devicesSvg} height={500}alt='img'/>
 		</div>
