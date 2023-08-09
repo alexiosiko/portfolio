6fbfba03ee705fe502889e99a235a6cf1e386a21
Alexi <you@example.com>
2023-08-08 22:05:50 -0700

    Intro and mobile compatible

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -18,10 +18,10 @@ export default function Intro() {
 			</h1>
 			<h3 style={{ lineHeight: 1.7, marginTop: 50}}>
 				Over the past 2 years, I have worked on various projects
-				developing my skills and delivering exciting outcomes.
-				If you're looking for a creative and enthusiastic
-				developer, let's connect and build amazing projects
-				together!
+				in Unity C# and FullStack with React.js and created some exciting outcomes.
+				If you're looking for a fast-learning and enthusiastic
+				developer, let's connect to collaborate on some amazing
+				projects!
 			</h3>
 		</div>
 	)
