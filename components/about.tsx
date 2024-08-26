ea6aa722b42f34cac0f47eb1ac8b4d7fd406da0b
Alexi <jemimassyrup@gmail.com>
2024-08-25 20:54:03 -0700

    resume and about

--- components/about.tsx
+++ components/about.tsx
@@ -18,18 +18,18 @@ export default function About() {
       id="about"
     >
 		<p className="mb-6">
-			I've started off programming in Unity with game development, where I made sprites and simple animations to create puzzles.
-			Seeing <span className="font-bold">simple </span>pieces of code that make my visions come to life is what got me <span className="font-bold">addicted</span> to programming; <br /><span className="font-bold"> I haven't stopped since </span>.. 
-		</p>
-		<p className="mb-6">
-			College then taught me the basics of React, where I then jumped <span className="font-bold">10 steps</span> ahead and learned <span className="font-bold">advanced full stack developement </span>
-			at my own pace. It's become to the point where if I can imagine it, <span className="font-bold">I can make it</span>. Since I've learned areas such as Next.js, React-Native, Mongodb, Figma ... etc,
-			the doors really open and technology goes from once being <span className="font-bold">"that magic that the computer creates"</span> to <span className="font-bold">"the magic that I create"</span>.  
+			I've started off programming in Unity with <span className="font-bold">game development</span>, where I worked with both 2D and 3D enviroments.
+			I quickly got addicted to seeing what <span className="font-bold">amazing</span> things I can do with a <span className="font-bold">little</span> amount of code<span className="font-bold"></span>; <br /><span className="font-bold"> I haven't stopped since </span>.. 
 		</p>
 		<p className="mb-6">
 			Programming is puzzles to me, and I <span className="font-bold">love</span> solving puzzles.
+			Ever since I've learned such advanced topics,
+			the doors really open and technology goes from once being <span className="font-bold">"that magic that the computer creates"</span> to <span className="font-bold">"the magic that I create"</span>.  
+		</p>
+			
+		<p>
 			My <span className="font-bold">dream</span> is to become a <span className="font-bold">professional software developer</span> and to be working
-			with <span className="font-bold">successful people</span> that <br /> <span className="font-bold">I am proud to work with</span>.
+			with <span className="font-bold">successful people</span> that <span className="font-bold">I am proud to work with</span>.
 		</p>
 	</motion.section>
   );
